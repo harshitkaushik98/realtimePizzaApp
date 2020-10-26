9161b3a35acf50e767a4f2dd4b357a93ec25d1c2
Harshit Kaushik <harshitkaushik98@gmail.com>
2020-10-26 19:40:30 +0530

    initial

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express() //all functionalities provided by express is used with help of this variable.
+const ejs = require('ejs')
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
+
+app.get('/',(req,res)=>{
+    res.render("home")
+})
+
+app.use(expressLayout)
+
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs')
+
+app.listen(3000,()=>{
+    console.log("Listening on port 3000")
+})
