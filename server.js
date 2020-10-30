de19969ac69800c9b0895aaf0a112ee5dc20a754
Harshit Kaushik <harshitkaushik98@gmail.com>
2020-10-30 23:48:27 +0530

    add to cart

--- server.js
+++ server.js
@@ -1,11 +1,53 @@
+require('dotenv').config()
 const express = require('express')
 const app = express() //all functionalities provided by express is used with help of this variable.
 const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
+const mongoose = require('mongoose');
+const session = require('express-session')
+const flash = require('express-flash')
+const MongoDbStore = require('connect-mongo')(session)
 
+
+//Database Connection
+const url = 'mongodb://localhost/pizza';
+mongoose.connect(url,{useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true,
+useFindAndModify: true });
+const connection = mongoose.connection;
+connection.once('open',()=>{
+    console.log('Database Connected...');
+}).catch(err => {
+    console.log('Connection Failed...');
+});
+
+//Session Store
+let mongoStore = new MongoDbStore({
+    mongooseConnection: connection,
+    collection: 'sessions'
+})
+
+//Session config
+
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    store: mongoStore,
+    saveUninitialized: false,
+    cookie: {maxAge: 1000 * 60 * 60 * 24}//24 hrs in milli second
+}))
+
+app.use(flash())
 //Assets
 app.use(express.static('public'))
+
+app.use(express.json())
+
+//Global Middleware
+app.use((req,res,next)=>{
+    res.locals.session = req.session
+    next()
+})
 
 
 app.use(expressLayout)
@@ -13,21 +55,9 @@ app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 
-app.get('/',(req,res)=>{
-    res.render("home")
-})
+require('./routes/web')(app);
 
-app.get('/cart',(req,res)=>{
-    res.render("customers/cart")
-})
 
-app.get('/login',(req,res)=>{
-    res.render("auth/login")
-})
-
-app.get('/register',(req,res)=>{
-    res.render("auth/register")
-})
 
 
 app.listen(3000,()=>{
